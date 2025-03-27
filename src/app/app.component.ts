fce70200371cfc0ada8b9697aa6dec8de90c1e26
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-03-27 12:37:49 -0300

    chore: :package: add tailwindCSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,9 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{ title }}!</h1>
-
+    <h1 class="text-3xl font-bold underline">Welcome to {{ title }}!</h1>
     <router-outlet />
-  `,
-  styles: []
+  `
 })
 export class AppComponent {
   title = 'poke-binder';
